3b60be058d241bc178f0a8e5a0ad6891261b8920
AgsBajs <bajajagampreet@gmail.com>
2024-10-28 14:09:20 -0400

    fixing httpS from http for security of website

--- src/components/airpolution.jsx
+++ src/components/airpolution.jsx
@@ -9,7 +9,7 @@ const AirPolution = ({ lat, lon, onAQIUpdate, onAirPolutionStatsUpdate}) => {
         if (lat && lon) {
             const fetchAirPolutionData = async () => {
                 const apiKey = '3de7efcab1d779a860b3887b098d516d'; // Replace with your API key
-                const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`; // 'units=metric' for Celsius
+                const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`; // 'units=metric' for Celsius
                 try {
                     const response = await axios.get(url);
                     const aqi = response.data.list[0].main.aqi;
