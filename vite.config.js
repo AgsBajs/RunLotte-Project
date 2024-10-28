5592ea31f47f45ec4ddd2e03ec3d492d31252961
AgsBajs <bajajagampreet@gmail.com>
2024-10-27 23:50:30 -0400

    updated vite config with axios

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom', 'styled-components', 'simple-icons'],
+      external: ['react-router-dom', 'styled-components', 'simple-icons', 'axios'],
     },
   },
   server: { 
