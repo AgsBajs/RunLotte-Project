f23a731f9d4344cd31ea84a4fd1b7cd0ba52f125
AgsBajs <bajajagampreet@gmail.com>
2024-10-27 23:32:48 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+import Landing from './Components/Landing';
+import './App.css';
+import VideoBackground from './Components/VideoBackground';
+import Map from './components/Adithya-Page/Map';
+import RoutePlanner from './components/Adithya-Page/Planner';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
