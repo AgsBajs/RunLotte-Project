dc39fbbfd4532947626bc094086587d35b3f7dda
AgsBajs <bajajagampreet@gmail.com>
2024-10-28 13:42:29 -0400

    re install axios and fixed icon imports

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
-import { siX, siInstagram, siLinkedin } from 'simple-icons';
+import { siX } from 'simple-icons';
 
 const Social = () => {
   return (
