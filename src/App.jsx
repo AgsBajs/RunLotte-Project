a7677568add4897f02a3553dbbccda32b982a4b3
AgsBajs <bajajagampreet@gmail.com>
2024-10-27 23:38:30 -0400

    fixing components import in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
-import Landing from './Components/Landing';
+import Landing from './components/Landing';
 import './App.css';
-import VideoBackground from './Components/VideoBackground';
 import Map from './components/Adithya-Page/Map';
-import RoutePlanner from './components/Adithya-Page/Planner';
 
 function App() {
   const [count, setCount] = useState(0);
